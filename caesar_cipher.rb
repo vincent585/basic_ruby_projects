ff18945dc0f753a6fac0adefc11c888e189407e4
vincent585 <58029655+vincent585@users.noreply.github.com>
2020-08-31 21:03:44 +0100

    Add files via upload

---
+++ caesar_cipher.rb
@@ -0,0 +1,17 @@
+def caesar_cipher(string, shift_factor)
+  character_values = string.split("").map do |char|
+    if /\W/ =~ char
+      char
+    else
+      offset = char =~ /[A-Z]/ ? 65 : 97
+      (((char.ord - offset + shift_factor) % 26) + offset).chr
+    end
+  end
+
+  character_values.join
+
+
+end
+
+
+p caesar_cipher("What a string!", 5)
