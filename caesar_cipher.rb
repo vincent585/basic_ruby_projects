4f203ae1ece94c3b3c2e4c7e16bef7a4e7d03e65
vincent585 <58029655+vincent585@users.noreply.github.com>
2020-12-16 06:54:28 +0000

    wrapped method in Cipher class

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,14 @@
-def caesar_cipher(string, shift_factor)
-  character_values = string.split("").map do |char|
-    if /\W/ =~ char
-      char
-    else
-      offset = char =~ /[A-Z]/ ? 65 : 97
-      (((char.ord - offset + shift_factor) % 26) + offset).chr
+class Cipher
+  def caesar_cipher(string, shift_factor)
+    character_values = string.split('').map do |char|
+      if /\W/ =~ char
+        char
+      else
+        offset = char =~ /[A-Z]/ ? 65 : 97
+        (((char.ord - offset + shift_factor) % 26) + offset).chr
+      end
     end
-  end
 
-  character_values.join
+    character_values.join
+  end
 end
